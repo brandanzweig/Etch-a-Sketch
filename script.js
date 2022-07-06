2a808fd32d158c2efc923ca98a54b4dd8b3f75c3
Brandan Zweig <brandanzweig@gmail.com>
2022-07-06 15:35:29 -0400

    Add grid

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let container = document.querySelector(".container");
 
-function addDiv(numberOfDivsToCreate) {
-    let gridSize = 16;
+let gridSize = 16;
 
-    // create grid squares & add to container
-    while (gridSize > 0) {
-        var newDiv = document.createElement('div'); 	
-        container.appendChild(newDiv); 
-        newDiv.classList.add('grid'); 
-        newDiv.setAttribute = ("style", "height: 16px; width: 16px; border: 1px solid black;");             
-        gridSize--; 
-    }
+
+    var newDiv = document.createElement('div'); 	
+    container.appendChild(newDiv); 
+    newDiv.classList.add('grid'); 
+    newDiv.setAttribute = ("style", "height: 16px; width: 16px; border: 1px solid black;");             
+    gridSize--; 
+
 
     let gridCells = document.querySelectorAll('.grid'); 
-}
+
+
