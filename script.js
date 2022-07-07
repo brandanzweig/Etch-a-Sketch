7ab96a8f6e9237c8d1448dad0036f9cf964b69fe
Brandan Zweig <brandanzweig@gmail.com>
2022-07-07 16:51:14 -0400

    Grid without bottom contain

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-let container = document.querySelector(".container");
+const container = document.querySelector('.container'); 
+const button = document.getElementById('clearButton'); 
 
-let gridSize = 16;
+function addDiv(numberOfDivsToCreate) {    
+    let gridCellDimensions = ((600 / numberOfDivsToCreate) - 2).toFixed(2); 
+    let gridSize = Math.pow(numberOfDivsToCreate, 2); 
 
+    // create grid squares & add to container
+    while (gridSize > 0) {
+        const newDiv = document.createElement('div'); 	
+        container.appendChild(newDiv); 
+        newDiv.classList.add('grid'); 
+        newDiv.style.height = gridCellDimensions + 'px';  
+        newDiv.style.width = gridCellDimensions + 'px'; 
+        newDiv.style.border = '1px solid black';             
+        gridSize--; 
+    }
 
-    var newDiv = document.createElement('div'); 	
-    container.appendChild(newDiv); 
-    newDiv.classList.add('grid'); 
-    newDiv.setAttribute = ("style", "height: 16px; width: 16px; border: 1px solid black;");             
-    gridSize--; 
+    const gridCells = document.querySelectorAll('.grid'); 
+    gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor)); 
+    console.log(gridCellDimensions);
+    console.log('Grid has been created!!')
+}
 
+// change grid square color to red
+function changeColor() {
+    this.style.backgroundColor = '#ff9999'; 
+}
 
-    let gridCells = document.querySelectorAll('.grid'); 
+// clear grid + prompt for new grid size
+function clear() {    	
+    let reqGridSize = prompt('How many squares per side?'); 
 
+    if (reqGridSize >= 1 && reqGridSize <= 100) { 
+        while (container.hasChildNodes()) {
+            container.removeChild(container.lastChild); // removes all grid squares
+        }
+        addDiv(reqGridSize); // create new grid 
+    } else {
+        alert ('Choose a number between 1-100'); 
+        clear();
+    }
+}
 
+button.addEventListener('click', clear); 
+window.onload = addDiv(16); // on page load, create a 16 x 16 grid
