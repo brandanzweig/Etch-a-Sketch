c1ae7040049064a7aa67ba3a2ecf6a2ba6665d12
Brandan Zweig <brandanzweig@gmail.com>
2022-07-17 22:22:57 -0400

    Add reset button with prompt for grid size

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const sizeOfGrid = 16
 const container = document.querySelector(".container");
+const resetButton = document.querySelector("button");
 
-const createGrid = (NumOfGridSq) => {
-    for (let i = 0; i < NumOfGridSq; i++) {
+const createGrid = (numOfGridSq) => {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("wrapper")
+
+    for (let i = 0; i < numOfGridSq; i++) {
         const row = document.createElement("div");
         row.classList.add("grid-row")
 
-        for (let j = 0; j < NumOfGridSq; j++) {
-            const widthAndHeight = 1000 / sizeOfGrid;
+        for (let j = 0; j < numOfGridSq; j++) {
+            const widthAndHeight = 1000 / numOfGridSq;
             const gridBox = document.createElement("div")
             gridBox.classList.add("grid-box")
             gridBox.style.width = `${widthAndHeight}px`
@@ -19,8 +22,24 @@ const createGrid = (NumOfGridSq) => {
             row.appendChild(gridBox)
         }
 
-    container.appendChild(row)
+        wrapper.appendChild(row)
     }
+    container.appendChild(wrapper)
 }
 
-createGrid(sizeOfGrid)
+resetButton.addEventListener("click", () => {
+    let userSize = Number(prompt("what dimensions do you want for the new grid?"))
+
+        while (userSize > 100) {
+        userSize = Number(prompt("Pick a number less than 100"))
+        }
+    
+    const wrapper = document.querySelector(".wrapper")
+    
+    if (!wrapper) {
+        createGrid(userSize)
+    } else {
+        wrapper.remove()
+        createGrid(userSize) 
+    }
+})
