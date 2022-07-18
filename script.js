41759effeed3e0436c8cf225192a90e3997a8220
Brandan Zweig <brandanzweig@gmail.com>
2022-07-17 20:58:20 -0400

    Add dynamic height & width within grid

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const createGrid = (NumOfGridSq) => {
         row.classList.add("grid-row")
 
         for (let j = 0; j < NumOfGridSq; j++) {
+            const widthAndHeight = 1000 / sizeOfGrid;
             const gridBox = document.createElement("div")
             gridBox.classList.add("grid-box")
+            gridBox.style.width = `${widthAndHeight}px`
+            gridBox.style.height = `${widthAndHeight}px`
             row.appendChild(gridBox)
         }
 
